b4210700ca427a7bcd28f6a2ad88fd4f07e83291
Kamil <kamilkwasny92@gmail.com>
2019-01-28 19:34:41 +0100

    TOOLS - UI - CategoryCard

--- CILantroTools/cilantro-tools-web-ui/src/components/shared/categories/CilCategoryCard.tsx
+++ CILantroTools/cilantro-tools-web-ui/src/components/shared/categories/CilCategoryCard.tsx
@@ -1,13 +1,40 @@
 import React, { StatelessComponent } from 'react';
 
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Theme,
+  Typography
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
 import CategoryReadModel from '../../../api/read-models/categories/CategoryReadModel';
+import CilAddSubcategoryButton from './CilAddSubcategoryButton';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  cardActions: {
+    justifyContent: 'flex-end'
+  }
+}));
 
 interface CilCategoryCardProps {
   category: CategoryReadModel;
 }
 
 const CilCategoryCard: StatelessComponent<CilCategoryCardProps> = props => {
-  return <div>CilCategoryCard</div>;
+  const classes = useStyles();
+
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="h2">{props.category.name}</Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <CilAddSubcategoryButton />
+      </CardActions>
+    </Card>
+  );
 };
 
 export default CilCategoryCard;
