7c7a0387497fbd6de63e59bb5eb67f71c6eea1e2
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-02-16 22:19:13 +0100

    CILantro - initial project added

---
+++ CILantro/CILantro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CILantro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CILantro!");
+        }
+    }
+}
