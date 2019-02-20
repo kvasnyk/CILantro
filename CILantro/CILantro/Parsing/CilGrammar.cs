57aefb4efcdf3d0afda633ab70960c47d487bae4
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-02-20 14:02:05 +0100

    ANTRO - parse single line comments (//)

--- CILantro/CILantro/Parsing/CilGrammar.cs
+++ CILantro/CILantro/Parsing/CilGrammar.cs
@@ -7,6 +7,10 @@ namespace CILantro.Parsing
         public CilGrammar()
             : base(true)
         {
+            var SINGLE_LINE_COMMENT = new CommentTerminal("SINGLE_LINE_COMMENT", "//", "\n", "\r\n");
+
+            NonGrammarTerminals.Add(SINGLE_LINE_COMMENT);
+
             var decls = new NonTerminal("decls");
             decls.Rule = Empty;
 
