e6be7889b091c3ba40d6471edc993b4a1be6db3a
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-02-20 13:10:55 +0100

    ANTRO - parse command line args

---
+++ CILantro/CILantro/ConsoleClient/CILantroArgs.cs
@@ -0,0 +1,19 @@
+﻿using CommandLineParser.Arguments;
+
+namespace CILantro.ConsoleClient
+{
+    public class CILantroArgs
+    {
+        [ValueArgument(typeof(string), 'f', "fileName", AllowMultiple = false)]
+        public string FileName { get; set; }
+
+        public static CILantroArgs Parse(string[] args)
+        {
+            var result = new CILantroArgs();
+            var parser = new CommandLineParser.CommandLineParser();
+            parser.ExtractArgumentAttributes(result);
+            parser.ParseCommandLine(args);
+            return result;
+        }
+    }
+}
--- CILantro/CILantro/Program.cs
+++ CILantro/CILantro/Program.cs
@@ -1,11 +1,16 @@
-﻿using CILantro.Engine;
+﻿using CILantro.ConsoleClient;
+using CILantro.Engine;
+using System;
 
 namespace CILantro
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] originalArgs)
         {
+            var args = CILantroArgs.Parse(originalArgs);
+            Console.WriteLine(args.FileName);
+
             var engine = new CilEngine("test");
             engine.Parse();
         }
