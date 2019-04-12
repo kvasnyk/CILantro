1ed2d452f5211f914325d4fe8943a693984c0654
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-04-12 11:16:51 +0200

    TESTS - read / write byte

---
+++ tests/CILantroTests_CSharpFeatures/CSF_DataTypes_Byte/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSF_DataTypes_Byte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = byte.Parse(Console.ReadLine());
+            Console.WriteLine(data);
+        }
+    }
+}
