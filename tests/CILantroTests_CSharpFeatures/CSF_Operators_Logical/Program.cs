d1b96cac04fb7dd65a4041f48d1bfed98397526c
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-05-05 15:11:58 +0200

    TESTS - C# logical operators

---
+++ tests/CILantroTests_CSharpFeatures/CSF_Operators_Logical/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSF_Operators_Logical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = bool.Parse(Console.ReadLine());
+            var b = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(!a);
+            Console.WriteLine(!b);
+            Console.WriteLine(a && b);
+            Console.WriteLine(a || b);
+        }
+    }
+}
