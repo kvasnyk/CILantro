311c44ba2e3db732d02da4a5410b9f788c5771ab
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-05-06 11:28:34 +0200

    TESTS - C# assign operators

--- tests/CILantroTests_CSharpFeatures/CSF_Operators_Assign/Program.cs
+++ tests/CILantroTests_CSharpFeatures/CSF_Operators_Assign/Program.cs
@@ -3,47 +3,47 @@
 namespace CSF_Operators_Assign
 {
     class Program
-    {
-        static void Main(string[] args)
         {
-            var a = sbyte.Parse(Console.ReadLine());
-            var b = short.Parse(Console.ReadLine());
-            var c = int.Parse(Console.ReadLine());
-            var d = long.Parse(Console.ReadLine());
-            var e = byte.Parse(Console.ReadLine());
-            var f = ushort.Parse(Console.ReadLine());
-            var g = uint.Parse(Console.ReadLine());
-            var h = ulong.Parse(Console.ReadLine());
-            var i = char.Parse(Console.ReadLine());
-            var j = float.Parse(Console.ReadLine());
-            var k = double.Parse(Console.ReadLine());
-            var l = decimal.Parse(Console.ReadLine());
+            static void Main(string[] args)
+            {
+                var a = sbyte.Parse(Console.ReadLine());
+                var b = short.Parse(Console.ReadLine());
+                var c = int.Parse(Console.ReadLine());
+                var d = long.Parse(Console.ReadLine());
+                var e = byte.Parse(Console.ReadLine());
+                var f = ushort.Parse(Console.ReadLine());
+                var g = uint.Parse(Console.ReadLine());
+                var h = ulong.Parse(Console.ReadLine());
+                var i = char.Parse(Console.ReadLine());
+                var j = float.Parse(Console.ReadLine());
+                var k = double.Parse(Console.ReadLine());
+                var l = decimal.Parse(Console.ReadLine());
 
-            var a2 = a;
-            var b2 = b;
-            var c2 = c;
-            var d2 = d;
-            var e2 = e;
-            var f2 = f;
-            var g2 = g;
-            var h2 = h;
-            var i2 = i;
-            var j2 = j;
-            var k2 = k;
-            var l2 = l;
+                var a2 = a;
+                var b2 = b;
+                var c2 = c;
+                var d2 = d;
+                var e2 = e;
+                var f2 = f;
+                var g2 = g;
+                var h2 = h;
+                var i2 = i;
+                var j2 = j;
+                var k2 = k;
+                var l2 = l;
 
-            Console.WriteLine(a2);
-            Console.WriteLine(b2);
-            Console.WriteLine(c2);
-            Console.WriteLine(d2);
-            Console.WriteLine(e2);
-            Console.WriteLine(f2);
-            Console.WriteLine(g2);
-            Console.WriteLine(h2);
-            Console.WriteLine(i2);
-            Console.WriteLine(j2);
-            Console.WriteLine(k2);
-            Console.WriteLine(l2);
+                Console.WriteLine(a2);
+                Console.WriteLine(b2);
+                Console.WriteLine(c2);
+                Console.WriteLine(d2);
+                Console.WriteLine(e2);
+                Console.WriteLine(f2);
+                Console.WriteLine(g2);
+                Console.WriteLine(h2);
+                Console.WriteLine(i2);
+                Console.WriteLine(j2);
+                Console.WriteLine(k2);
+                Console.WriteLine(l2);
+            }
         }
     }
-}
---
+++ tests/CILantroTests_CSharpFeatures/CSF_Operators_ModAndAssign/Program.cs
@@ -0,0 +1,308 @@
+﻿using System;
+
+namespace CSF_Operators_ModAndAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = sbyte.Parse(Console.ReadLine());
+            var b = short.Parse(Console.ReadLine());
+            var c = int.Parse(Console.ReadLine());
+            var d = long.Parse(Console.ReadLine());
+            var e = byte.Parse(Console.ReadLine());
+            var f = ushort.Parse(Console.ReadLine());
+            var g = uint.Parse(Console.ReadLine());
+            var h = ulong.Parse(Console.ReadLine());
+            var i = char.Parse(Console.ReadLine());
+            var j = float.Parse(Console.ReadLine());
+            var k = double.Parse(Console.ReadLine());
+            var l = decimal.Parse(Console.ReadLine());
+
+            // a
+
+            var aa = a;
+
+            Console.Write(aa);
+            Console.Write(" ");
+            aa %= a;
+            Console.Write(aa);
+            Console.Write(" ");
+            Console.WriteLine();
+
+            // b
+
+            var bb = b;
+
+            Console.Write(bb);
+            Console.Write(" ");
+            bb %= a;
+            Console.Write(bb);
+            Console.Write(" ");
+            bb %= b;
+            Console.Write(bb);
+            Console.Write(" ");
+            bb %= e;
+            Console.Write(bb);
+            Console.Write(" ");
+            Console.WriteLine();
+
+            // c
+
+            var cc = c;
+
+            Console.Write(cc);
+            Console.Write(" ");
+            cc %= a;
+            Console.Write(cc);
+            Console.Write(" ");
+            cc %= b;
+            Console.Write(cc);
+            Console.Write(" ");
+            cc %= c;
+            Console.Write(cc);
+            Console.Write(" ");
+            cc %= e;
+            Console.Write(cc);
+            Console.Write(" ");
+            cc %= f;
+            Console.Write(cc);
+            Console.Write(" ");
+            cc %= i;
+            Console.Write(cc);
+            Console.Write(" ");
+            Console.WriteLine();
+
+            // d
+
+            var dd = d;
+
+            Console.Write(dd);
+            Console.Write(" ");
+            dd %= a;
+            Console.Write(dd);
+            Console.Write(" ");
+            dd %= b;
+            Console.Write(dd);
+            Console.Write(" ");
+            dd %= c;
+            Console.Write(dd);
+            Console.Write(" ");
+            dd %= d;
+            Console.Write(dd);
+            Console.Write(" ");
+            dd %= e;
+            Console.Write(dd);
+            Console.Write(" ");
+            dd %= f;
+            Console.Write(dd);
+            Console.Write(" ");
+            dd %= g;
+            Console.Write(dd);
+            Console.Write(" ");
+            dd %= i;
+            Console.Write(dd);
+            Console.Write(" ");
+            Console.WriteLine();
+
+            // e
+
+            var ee = e;
+
+            Console.Write(ee);
+            Console.Write(" ");
+            ee %= e;
+            Console.Write(ee);
+            Console.Write(" ");
+            Console.WriteLine();
+
+            // f
+
+            var ff = f;
+
+            Console.Write(ff);
+            Console.Write(" ");
+            ff %= e;
+            Console.Write(ff);
+            Console.Write(" ");
+            ff %= f;
+            Console.Write(ff);
+            Console.Write(" ");
+            ff %= i;
+            Console.Write(ff);
+            Console.Write(" ");
+            Console.WriteLine();
+
+            // g
+
+            var gg = g;
+
+            Console.Write(gg);
+            Console.Write(" ");
+            gg %= e;
+            Console.Write(gg);
+            Console.Write(" ");
+            gg %= f;
+            Console.Write(gg);
+            Console.Write(" ");
+            gg %= g;
+            Console.Write(gg);
+            Console.Write(" ");
+            gg %= i;
+            Console.Write(gg);
+            Console.Write(" ");
+            Console.WriteLine();
+
+            // h
+
+            var hh = h;
+
+            Console.Write(hh);
+            Console.Write(" ");
+            hh %= e;
+            Console.Write(hh);
+            Console.Write(" ");
+            hh %= f;
+            Console.Write(hh);
+            Console.Write(" ");
+            hh %= g;
+            Console.Write(hh);
+            Console.Write(" ");
+            hh %= h;
+            Console.Write(hh);
+            Console.Write(" ");
+            hh %= i;
+            Console.Write(hh);
+            Console.Write(" ");
+            Console.WriteLine();
+
+            // i
+
+            var ii = i;
+
+            Console.Write(ii);
+            Console.Write(" ");
+            ii %= i;
+            Console.Write(ii);
+            Console.Write(" ");
+            Console.WriteLine();
+
+            // j
+
+            var jj = j;
+
+            Console.Write(jj);
+            Console.Write(" ");
+            jj %= a;
+            Console.Write(jj);
+            Console.Write(" ");
+            jj %= b;
+            Console.Write(jj);
+            Console.Write(" ");
+            jj %= c;
+            Console.Write(jj);
+            Console.Write(" ");
+            jj %= d;
+            Console.Write(jj);
+            Console.Write(" ");
+            jj %= e;
+            Console.Write(jj);
+            Console.Write(" ");
+            jj %= f;
+            Console.Write(jj);
+            Console.Write(" ");
+            jj %= g;
+            Console.Write(jj);
+            Console.Write(" ");
+            jj %= h;
+            Console.Write(jj);
+            Console.Write(" ");
+            jj %= i;
+            Console.Write(jj);
+            Console.Write(" ");
+            jj %= j;
+            Console.Write(jj);
+            Console.Write(" ");
+            Console.WriteLine();
+
+            // k
+
+            var kk = k;
+
+            Console.Write(kk);
+            Console.Write(" ");
+            kk %= a;
+            Console.Write(kk);
+            Console.Write(" ");
+            kk %= b;
+            Console.Write(kk);
+            Console.Write(" ");
+            kk %= c;
+            Console.Write(kk);
+            Console.Write(" ");
+            kk %= d;
+            Console.Write(kk);
+            Console.Write(" ");
+            kk %= e;
+            Console.Write(kk);
+            Console.Write(" ");
+            kk %= f;
+            Console.Write(kk);
+            Console.Write(" ");
+            kk %= g;
+            Console.Write(kk);
+            Console.Write(" ");
+            kk %= h;
+            Console.Write(kk);
+            Console.Write(" ");
+            kk %= i;
+            Console.Write(kk);
+            Console.Write(" ");
+            kk %= j;
+            Console.Write(kk);
+            Console.Write(" ");
+            kk %= k;
+            Console.Write(kk);
+            Console.Write(" ");
+            Console.WriteLine();
+
+            // l
+
+            var ll = l;
+
+            Console.Write(ll);
+            Console.Write(" ");
+            ll %= a;
+            Console.Write(ll);
+            Console.Write(" ");
+            ll %= b;
+            Console.Write(ll);
+            Console.Write(" ");
+            ll %= c;
+            Console.Write(ll);
+            Console.Write(" ");
+            ll %= d;
+            Console.Write(ll);
+            Console.Write(" ");
+            ll %= e;
+            Console.Write(ll);
+            Console.Write(" ");
+            ll %= f;
+            Console.Write(ll);
+            Console.Write(" ");
+            ll %= g;
+            Console.Write(ll);
+            Console.Write(" ");
+            ll %= h;
+            Console.Write(ll);
+            Console.Write(" ");
+            ll %= i;
+            Console.Write(ll);
+            Console.Write(" ");
+            ll %= l;
+            Console.Write(ll);
+            Console.Write(" ");
+            Console.WriteLine();
+        }
+    }
+}
