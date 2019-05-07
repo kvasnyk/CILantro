b27148c0f80263d83a1db33be9a1f15481b4b924
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-05-07 09:44:46 +0200

    TESTS - if equal / if not equal

---
+++ tests/CILantroTests_CSharpFeatures/CSF_DecisionMaking_IfNotEqual/Program.cs
@@ -0,0 +1,206 @@
+﻿
+using System;
+
+namespace CSF_DecisionMaking_IfNotEqual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line1 = Console.ReadLine().Split(' ');
+            var a = sbyte.Parse(line1[0]);
+
+            var line2 = Console.ReadLine().Split(' ');
+            var b = short.Parse(line2[0]);
+
+            var line3 = Console.ReadLine().Split(' ');
+            var c = int.Parse(line3[0]);
+
+            var line4 = Console.ReadLine().Split(' ');
+            var d = long.Parse(line4[0]);
+
+            var line5 = Console.ReadLine().Split(' ');
+            var e = byte.Parse(line5[0]);
+
+            var line6 = Console.ReadLine().Split(' ');
+            var f = ushort.Parse(line6[0]);
+
+            var line7 = Console.ReadLine().Split(' ');
+            var g = uint.Parse(line7[0]);
+
+            var line8 = Console.ReadLine().Split(' ');
+            var h = ulong.Parse(line8[0]);
+
+            var line9 = Console.ReadLine().Split(' ');
+            var i = char.Parse(line9[0]);
+
+            var line10 = Console.ReadLine().Split(' ');
+            var j = float.Parse(line10[0]);
+
+            var line11 = Console.ReadLine().Split(' ');
+            var k = double.Parse(line11[0]);
+
+            var line12 = Console.ReadLine().Split(' ');
+            var l = decimal.Parse(line12[0]);
+
+            if (a != a) Console.Write("Y ");
+            else Console.Write("N ");
+
+            if (a != a) Console.Write("Y "); else Console.Write("N ");
+            if (a != b) Console.Write("Y "); else Console.Write("N ");
+            if (a != c) Console.Write("Y "); else Console.Write("N ");
+            if (a != d) Console.Write("Y "); else Console.Write("N ");
+            if (a != e) Console.Write("Y "); else Console.Write("N ");
+            if (a != f) Console.Write("Y "); else Console.Write("N ");
+            if (a != g) Console.Write("Y "); else Console.Write("N ");
+            if (a != i) Console.Write("Y "); else Console.Write("N ");
+            if (a != j) Console.Write("Y "); else Console.Write("N ");
+            if (a != k) Console.Write("Y "); else Console.Write("N ");
+            if (a != l) Console.Write("Y "); else Console.Write("N ");
+            Console.WriteLine();
+
+            if (b != a) Console.Write("Y "); else Console.Write("N ");
+            if (b != b) Console.Write("Y "); else Console.Write("N ");
+            if (b != c) Console.Write("Y "); else Console.Write("N ");
+            if (b != d) Console.Write("Y "); else Console.Write("N ");
+            if (b != e) Console.Write("Y "); else Console.Write("N ");
+            if (b != f) Console.Write("Y "); else Console.Write("N ");
+            if (b != g) Console.Write("Y "); else Console.Write("N ");
+            if (b != i) Console.Write("Y "); else Console.Write("N ");
+            if (b != j) Console.Write("Y "); else Console.Write("N ");
+            if (b != k) Console.Write("Y "); else Console.Write("N ");
+            if (b != l) Console.Write("Y "); else Console.Write("N ");
+            Console.WriteLine();
+
+            if (c != a) Console.Write("Y "); else Console.Write("N ");
+            if (c != b) Console.Write("Y "); else Console.Write("N ");
+            if (c != c) Console.Write("Y "); else Console.Write("N ");
+            if (c != d) Console.Write("Y "); else Console.Write("N ");
+            if (c != e) Console.Write("Y "); else Console.Write("N ");
+            if (c != f) Console.Write("Y "); else Console.Write("N ");
+            if (c != g) Console.Write("Y "); else Console.Write("N ");
+            if (c != i) Console.Write("Y "); else Console.Write("N ");
+            if (c != j) Console.Write("Y "); else Console.Write("N ");
+            if (c != k) Console.Write("Y "); else Console.Write("N ");
+            if (c != l) Console.Write("Y "); else Console.Write("N ");
+            Console.WriteLine();
+
+            if (d != a) Console.Write("Y "); else Console.Write("N ");
+            if (d != b) Console.Write("Y "); else Console.Write("N ");
+            if (d != c) Console.Write("Y "); else Console.Write("N ");
+            if (d != d) Console.Write("Y "); else Console.Write("N ");
+            if (d != e) Console.Write("Y "); else Console.Write("N ");
+            if (d != f) Console.Write("Y "); else Console.Write("N ");
+            if (d != g) Console.Write("Y "); else Console.Write("N ");
+            if (d != i) Console.Write("Y "); else Console.Write("N ");
+            if (d != j) Console.Write("Y "); else Console.Write("N ");
+            if (d != k) Console.Write("Y "); else Console.Write("N ");
+            if (d != l) Console.Write("Y "); else Console.Write("N ");
+            Console.WriteLine();
+
+            if (e != a) Console.Write("Y "); else Console.Write("N ");
+            if (e != b) Console.Write("Y "); else Console.Write("N ");
+            if (e != c) Console.Write("Y "); else Console.Write("N ");
+            if (e != d) Console.Write("Y "); else Console.Write("N ");
+            if (e != e) Console.Write("Y "); else Console.Write("N ");
+            if (e != f) Console.Write("Y "); else Console.Write("N ");
+            if (e != g) Console.Write("Y "); else Console.Write("N ");
+            if (e != h) Console.Write("Y "); else Console.Write("N ");
+            if (e != i) Console.Write("Y "); else Console.Write("N ");
+            if (e != j) Console.Write("Y "); else Console.Write("N ");
+            if (e != k) Console.Write("Y "); else Console.Write("N ");
+            if (e != l) Console.Write("Y "); else Console.Write("N ");
+            Console.WriteLine();
+
+            if (f != a) Console.Write("Y "); else Console.Write("N ");
+            if (f != b) Console.Write("Y "); else Console.Write("N ");
+            if (f != c) Console.Write("Y "); else Console.Write("N ");
+            if (f != d) Console.Write("Y "); else Console.Write("N ");
+            if (f != e) Console.Write("Y "); else Console.Write("N ");
+            if (f != f) Console.Write("Y "); else Console.Write("N ");
+            if (f != g) Console.Write("Y "); else Console.Write("N ");
+            if (f != h) Console.Write("Y "); else Console.Write("N ");
+            if (f != i) Console.Write("Y "); else Console.Write("N ");
+            if (f != j) Console.Write("Y "); else Console.Write("N ");
+            if (f != k) Console.Write("Y "); else Console.Write("N ");
+            if (f != l) Console.Write("Y "); else Console.Write("N ");
+            Console.WriteLine();
+
+            if (g != a) Console.Write("Y "); else Console.Write("N ");
+            if (g != b) Console.Write("Y "); else Console.Write("N ");
+            if (g != c) Console.Write("Y "); else Console.Write("N ");
+            if (g != d) Console.Write("Y "); else Console.Write("N ");
+            if (g != e) Console.Write("Y "); else Console.Write("N ");
+            if (g != f) Console.Write("Y "); else Console.Write("N ");
+            if (g != g) Console.Write("Y "); else Console.Write("N ");
+            if (g != h) Console.Write("Y "); else Console.Write("N ");
+            if (g != i) Console.Write("Y "); else Console.Write("N ");
+            if (g != j) Console.Write("Y "); else Console.Write("N ");
+            if (g != k) Console.Write("Y "); else Console.Write("N ");
+            if (g != l) Console.Write("Y "); else Console.Write("N ");
+            Console.WriteLine();
+
+            if (h != e) Console.Write("Y "); else Console.Write("N ");
+            if (h != f) Console.Write("Y "); else Console.Write("N ");
+            if (h != g) Console.Write("Y "); else Console.Write("N ");
+            if (h != h) Console.Write("Y "); else Console.Write("N ");
+            if (h != i) Console.Write("Y "); else Console.Write("N ");
+            if (h != j) Console.Write("Y "); else Console.Write("N ");
+            if (h != k) Console.Write("Y "); else Console.Write("N ");
+            if (h != l) Console.Write("Y "); else Console.Write("N ");
+            Console.WriteLine();
+
+            if (i != a) Console.Write("Y "); else Console.Write("N ");
+            if (i != b) Console.Write("Y "); else Console.Write("N ");
+            if (i != c) Console.Write("Y "); else Console.Write("N ");
+            if (i != d) Console.Write("Y "); else Console.Write("N ");
+            if (i != e) Console.Write("Y "); else Console.Write("N ");
+            if (i != f) Console.Write("Y "); else Console.Write("N ");
+            if (i != g) Console.Write("Y "); else Console.Write("N ");
+            if (i != h) Console.Write("Y "); else Console.Write("N ");
+            if (i != i) Console.Write("Y "); else Console.Write("N ");
+            if (i != j) Console.Write("Y "); else Console.Write("N ");
+            if (i != k) Console.Write("Y "); else Console.Write("N ");
+            if (i != l) Console.Write("Y "); else Console.Write("N ");
+            Console.WriteLine();
+
+            if (j != a) Console.Write("Y "); else Console.Write("N ");
+            if (j != b) Console.Write("Y "); else Console.Write("N ");
+            if (j != c) Console.Write("Y "); else Console.Write("N ");
+            if (j != d) Console.Write("Y "); else Console.Write("N ");
+            if (j != e) Console.Write("Y "); else Console.Write("N ");
+            if (j != f) Console.Write("Y "); else Console.Write("N ");
+            if (j != g) Console.Write("Y "); else Console.Write("N ");
+            if (j != h) Console.Write("Y "); else Console.Write("N ");
+            if (j != i) Console.Write("Y "); else Console.Write("N ");
+            if (j != j) Console.Write("Y "); else Console.Write("N ");
+            if (j != k) Console.Write("Y "); else Console.Write("N ");
+            Console.WriteLine();
+
+            if (k != a) Console.Write("Y "); else Console.Write("N ");
+            if (k != b) Console.Write("Y "); else Console.Write("N ");
+            if (k != c) Console.Write("Y "); else Console.Write("N ");
+            if (k != d) Console.Write("Y "); else Console.Write("N ");
+            if (k != e) Console.Write("Y "); else Console.Write("N ");
+            if (k != f) Console.Write("Y "); else Console.Write("N ");
+            if (k != g) Console.Write("Y "); else Console.Write("N ");
+            if (k != h) Console.Write("Y "); else Console.Write("N ");
+            if (k != i) Console.Write("Y "); else Console.Write("N ");
+            if (k != j) Console.Write("Y "); else Console.Write("N ");
+            if (k != k) Console.Write("Y "); else Console.Write("N ");
+            Console.WriteLine();
+
+            if (l != a) Console.Write("Y "); else Console.Write("N ");
+            if (l != b) Console.Write("Y "); else Console.Write("N ");
+            if (l != c) Console.Write("Y "); else Console.Write("N ");
+            if (l != d) Console.Write("Y "); else Console.Write("N ");
+            if (l != e) Console.Write("Y "); else Console.Write("N ");
+            if (l != f) Console.Write("Y "); else Console.Write("N ");
+            if (l != g) Console.Write("Y "); else Console.Write("N ");
+            if (l != h) Console.Write("Y "); else Console.Write("N ");
+            if (l != i) Console.Write("Y "); else Console.Write("N ");
+            if (l != l) Console.Write("Y "); else Console.Write("N ");
+            Console.WriteLine();
+        }
+    }
+}
