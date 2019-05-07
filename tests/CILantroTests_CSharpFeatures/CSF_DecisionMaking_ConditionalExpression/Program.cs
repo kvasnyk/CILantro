189a546d3dd064c7015611b1215fa7b7575eda37
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-05-07 21:02:53 +0200

    TESTS - new tests for for loop & conditional expressions

---
+++ tests/CILantroTests_CSharpFeatures/CSF_DecisionMaking_ConditionalExpression/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSF_DecisionMaking_ConditionalExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine().Split(' ');
+
+            var a = int.Parse(line[0]);
+            var b = int.Parse(line[1]);
+
+            var min = a <= b ? a : b;
+            var max = a >= b ? a : b;
+
+            Console.Write(min);
+            Console.Write(" ");
+            Console.WriteLine(max);
+        }
+    }
+}
---
+++ tests/CILantroTests_CSharpFeatures/CSF_Loops_For/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSF_Loops_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine().Split(' ');
+            var a = sbyte.Parse(line[0]);
+            var b = sbyte.Parse(line[1]);
+
+            var min = a <= b ? a : b;
+            var max = a >= b ? a : b;
+
+            for (short i = min; i <= max; i++)
+                Console.WriteLine(i);
+        }
+    }
+}
