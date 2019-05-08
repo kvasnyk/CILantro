bd92c4bd17f0a4c3872681cf46b2453748c73d2d
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-05-08 13:27:28 +0200

    TESTS - C# do...while loop

---
+++ tests/CILantroTests_CSharpFeatures/CSF_Loops_DoWhile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSF_Loops_DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = sbyte.Parse(Console.ReadLine());
+
+            long result = 1;
+
+            do
+            {
+                Console.WriteLine(result);
+                result *= 2;
+                n--;
+            } while (n >= 0);
+        }
+    }
+}
