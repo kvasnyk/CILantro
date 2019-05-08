6499e63c696d26f115b049ae648409e44da0c2de
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-05-08 13:19:10 +0200

    TESTS - C# while loop

---
+++ tests/CILantroTests_CSharpFeatures/CSF_Loops_While/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSF_Loops_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = short.Parse(Console.ReadLine());
+
+            while(n >= 0)
+            {
+                Console.WriteLine(n);
+                n--;
+            }
+        }
+    }
+}
