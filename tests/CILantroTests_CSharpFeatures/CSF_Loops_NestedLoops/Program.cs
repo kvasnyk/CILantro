93e5fb25edc1a8c6a528eaaec111cd65da408fa4
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-05-08 13:44:03 +0200

    TESTS - C# nested loops

---
+++ tests/CILantroTests_CSharpFeatures/CSF_Loops_NestedLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSF_Loops_NestedLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine().Split(' ');
+            var n = int.Parse(line[0]);
+            var m = int.Parse(line[1]);
+
+            for (int i = 0; i <= n; i++)
+            {
+                var j = 0;
+                while (j <= m)
+                {
+                    var r = i * j;
+                    Console.Write(r.ToString().PadLeft(6, ' '));
+                    j++;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
