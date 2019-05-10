55c1601bb72f8c041761fe19cce4c84fbbbcae75
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-05-10 14:07:37 +0200

    TESTS - C# loops - break

---
+++ tests/CILantroTests_CSharpFeatures/CSF_Loops_Break/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSF_Loops_Break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+
+            int n;
+
+            do
+            {
+                n = int.Parse(Console.ReadLine());
+                sum += n;
+            } while (n != 0);
+
+            Console.WriteLine(sum);
+        }
+    }
+}
