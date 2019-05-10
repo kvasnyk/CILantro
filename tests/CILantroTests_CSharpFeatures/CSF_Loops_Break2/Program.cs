f9f5ebfbe6bd4f623730459720237282a507cb73
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-05-10 23:18:32 +0200

    TESTS - C# loops - break & continue

---
+++ tests/CILantroTests_CSharpFeatures/CSF_Loops_Break2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSF_Loops_Break2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = 0;
+
+            while (true)
+            {
+                var n = int.Parse(Console.ReadLine());
+
+                if (n == 0)
+                    break;
+
+                sum += n;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ tests/CILantroTests_CSharpFeatures/CSF_Loops_Continue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSF_Loops_Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = 0;
+
+            while (true)
+            {
+                var n = int.Parse(Console.ReadLine());
+
+                if (n == 0)
+                    break;
+
+                if (n < 0)
+                    continue;
+
+                sum += n;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
