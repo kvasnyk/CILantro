8698bc945fa8ad65299bad9f94ff865f8d055191
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-05-11 03:35:44 +0200

    TESTS - C# int array

---
+++ tests/CILantroTests_CSharpFeatures/CSF_Arrays_Int/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSF_Arrays_Int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = byte.Parse(Console.ReadLine());
+
+            var arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+                arr[i] = int.Parse(Console.ReadLine());
+
+            for (int i = n - 1; i >= 0; i--)
+                Console.WriteLine(arr[i]);
+        }
+    }
+}
