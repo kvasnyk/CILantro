317ec9e59317cb6ec57a4fa31e632655b7dfae72
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-05-12 00:38:56 +0200

    TESTS - first unsafe code test

---
+++ tests/CILantroTests_CSharpFeatures/CSF_UnsafeCode_Pointer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSF_UnsafeCode_Pointer
+{
+    class Program
+    {
+        static unsafe void Main(string[] args)
+        {
+            var n = 20;
+            int* p = &n;
+
+            Console.WriteLine(n);
+            Console.WriteLine(*p);
+        }
+    }
+}
