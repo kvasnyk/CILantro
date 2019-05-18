80d8e674f7da8edcf4a446ab0b5f7ce64884cdf3
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-05-18 02:05:19 +0200

    TESTS - get field value

---
+++ tests/CILantroTests_CSharpFeatures/CSF_Classes_Field_GetValue/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CSF_Classes_Field_GetValue
+{
+    public class Test
+    {
+        public sbyte _fieldSbyte = -4;
+
+        public short _fieldShort = -12;
+
+        public int _fieldInt = -1023;
+
+        public long _fieldLong = -10432432423;
+
+        public byte _fieldByte = 4;
+
+        public ushort _fieldUshort = 12;
+
+        public uint _fieldUint = 1023;
+
+        public ulong _fieldUlong = 10432432423;
+
+        public char _fieldChar = 'g';
+
+        public float _fieldFloat = 3.14f;
+
+        public double _fieldDouble = 3.15;
+
+        public decimal _fieldDecimal = 3.17m;
+
+        public string _fieldString = "hello!";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Test();
+
+            Console.WriteLine(test._fieldSbyte);
+            Console.WriteLine(test._fieldShort);
+            Console.WriteLine(test._fieldInt);
+            Console.WriteLine(test._fieldLong);
+            Console.WriteLine(test._fieldByte);
+            Console.WriteLine(test._fieldUshort);
+            Console.WriteLine(test._fieldUint);
+            Console.WriteLine(test._fieldUlong);
+            Console.WriteLine(test._fieldChar);
+            Console.WriteLine(test._fieldFloat);
+            Console.WriteLine(test._fieldDouble);
+            Console.WriteLine(test._fieldDecimal);
+            Console.WriteLine(test._fieldString);
+        }
+    }
+}
--- tests/CILantroTests_CSharpFeatures/CSF_Classes_StaticField_SetValue/Program.cs
+++ tests/CILantroTests_CSharpFeatures/CSF_Classes_StaticField_SetValue/Program.cs
@@ -2,37 +2,37 @@
 
 namespace CSF_Classes_StaticField_SetValue
 {
+    public class Test
+    {
+        public static sbyte _fieldSbyte = -4;
+
+        public static short _fieldShort = -12;
+
+        public static int _fieldInt = -1023;
+
+        public static long _fieldLong = -10432432423;
+
+        public static byte _fieldByte = 4;
+
+        public static ushort _fieldUshort = 12;
+
+        public static uint _fieldUint = 1023;
+
+        public static ulong _fieldUlong = 10432432423;
+
+        public static char _fieldChar = 'g';
+
+        public static float _fieldFloat = 3.14f;
+
+        public static double _fieldDouble = 3.15;
+
+        public static decimal _fieldDecimal = 3.17m;
+
+        public static string _fieldString = "hello!";
+    }
+
     class Program
     {
-        public class Test
-        {
-            public static sbyte _fieldSbyte = -4;
-
-            public static short _fieldShort = -12;
-
-            public static int _fieldInt = -1023;
-
-            public static long _fieldLong = -10432432423;
-
-            public static byte _fieldByte = 4;
-
-            public static ushort _fieldUshort = 12;
-
-            public static uint _fieldUint = 1023;
-
-            public static ulong _fieldUlong = 10432432423;
-
-            public static char _fieldChar = 'g';
-
-            public static float _fieldFloat = 3.14f;
-
-            public static double _fieldDouble = 3.15;
-
-            public static decimal _fieldDecimal = 3.17m;
-
-            public static string _fieldString = "hello!";
-        }
-
         static void Main(string[] args)
         {
             var @sbyte = sbyte.Parse(Console.ReadLine());
