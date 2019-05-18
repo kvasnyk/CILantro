5e4529ab01075b5b41f37a1109332c046b150e82
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-05-18 16:43:51 +0200

    TESTS - 1-arg constructor

---
+++ tests/CILantroTests_CSharpFeatures/CSF_Classes_Constructor_1Args/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSF_Classes_Constructor_1Args
+{
+    public class Test
+    {
+        public int _n = -3;
+
+        public Test(int n)
+        {
+            _n += n;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var test = new Test(n);
+            Console.WriteLine(test._n);
+        }
+    }
+}
