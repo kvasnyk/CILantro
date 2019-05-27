8e808ca95f9b8174370167034dfb82d7a4eb3a5c
Kamil Kwaśny <kamilkwasny92@gmail.com>
2019-05-28 01:13:19 +0200

    TESTS - first exceptions test

---
+++ tests/CILantroTests_CSharpFeatures/CSF_Exceptions_SimpleCatch/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSF_Exceptions_SimpleCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var y = 2 - 2;
+                var x = 1 / y;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Exception caught!");
+            }
+        }
+    }
+}
