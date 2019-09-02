2bc3a949ab605d095bb6204a087d432d1d345f4a
kvasnyk <kamilkwasny92@gmail.com>
2019-09-02 06:30:47 +0200

    thesis + performance programs

---
+++ tests/CILantroTests_CSharpFeatures/CSF_Performance_Ackermann/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSF_Performance_Ackermann
+{
+    class Program
+    {
+        static int Ackermann(int m, int n)
+        {
+            if (m == 0)
+                return n + 1;
+            if (m > 0 && n == 0)
+                return Ackermann(m - 1, 1);
+            if (m > 0 && n > 0)
+                return Ackermann(m - 1, Ackermann(m, n - 1));
+
+            return 1;
+        }
+
+        static void Main(string[] args)
+        {
+            var m = int.Parse(Console.ReadLine());
+            var n = int.Parse(Console.ReadLine());
+            var ack = Ackermann(m, n);
+            Console.WriteLine(ack);
+        }
+    }
+}
---
+++ tests/CILantroTests_CSharpFeatures/CSF_Performance_Natural/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSF_Performance_Natural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+                Console.WriteLine(i);
+        }
+    }
+}
